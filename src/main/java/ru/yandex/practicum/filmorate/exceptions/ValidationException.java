43a766685d7892552cde4f71fef3bb796ed651a5
Vladimir <bistrvova@yandex.com>
2023-08-21 16:04:29 +0500

    style: Исправление по стилю #6

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-import org.springframework.http.HttpStatus;
-
 public class ValidationException extends Exception {
     public ValidationException(String reason) {
         super(reason);
